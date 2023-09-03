6f32c91c80bfb7078b4ea8e27078693b07587ddf
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-09-04 04:34:43 +0500

    Month update 2 (pylint error)

--- app.py
+++ app.py
@@ -16,10 +16,18 @@ class Month(Enum):
     Month by it's number
     """
 
-    DECEMBER = 12; JANUARY = 1; FEBRUARY = 2
-    MARCH = 3; APRIL = 4; MAY = 5
-    JUNE = 6; JULY = 7; AUGUST = 8
-    SEPTEMBER = 9; OCTOBER = 10; NOVEMBER = 11
+    DECEMBER = 12
+    JANUARY = 1
+    FEBRUARY = 2
+    MARCH = 3
+    APRIL = 4
+    MAY = 5
+    JUNE = 6
+    JULY = 7
+    AUGUST = 8
+    SEPTEMBER = 9
+    OCTOBER = 10
+    NOVEMBER = 11
 
 def check_folder(path):
     """
